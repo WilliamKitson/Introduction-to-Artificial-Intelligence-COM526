ea49e143db1660c8d5822d6495a6dc00fc7805bc
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-02 12:55:54 +0000

    main redefined

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cleaner import Cleaner
 
 cleaner = Cleaner(0, 0)
 
-for i in range(10):
+while cleaner.get_battery():
     cleaner.set_forwards(0)
     cleaner.cycle()
     print(f"cycle {cleaner.get_cycle()}: {cleaner.get_render()} ({cleaner.get_battery()}%)")
