dd4d424d95efd014c95b894571915d143a060d7f
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-02 10:39:17 +0000

    test_position_initialisation defined

--- cleaner.py
+++ cleaner.py
@@ -1,6 +1,10 @@
 class Cleaner:
-    def __init__(self):
+    def __init__(self, x_position, y_position):
         self.__battery = 100
+        self.__position = (x_position, y_position)
 
     def get_battery(self):
         return self.__battery
+
+    def get_position(self):
+        return self.__position
--- test_cleaner.py
+++ test_cleaner.py
@@ -1,4 +1,9 @@
 from cleaner import  Cleaner
 
 def test_battery_default_charge():
-    assert(Cleaner().get_battery() == 100)
+    assert(Cleaner(0, 0).get_battery() == 100)
+
+def test_position_initialisation():
+    for i in range(10):
+        for j in range(10):
+            assert(Cleaner(i, j).get_position() == (i, j))
