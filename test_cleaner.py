8ea9e236343ee8b7b5b4e6ba15f611a84d76c0b5
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-04 11:05:36 +0000

    test_scan_north defined

--- test_cleaner.py
+++ test_cleaner.py
@@ -209,3 +209,16 @@ def test_scan_west():
         cleaner.set_scan(1)
         cleaner.cycle()
         assert(cleaner.get_scan() == (0, -i - 1))
+
+def test_scan_north():
+    cleaner = Cleaner(0, 0)
+    cleaner.set_scan(0)
+    cleaner.cycle()
+    cleaner.cycle()
+    cleaner.cycle()
+    cleaner.cycle()
+
+    for i in range(1, 10):
+        cleaner.set_scan(1)
+        cleaner.cycle()
+        assert(cleaner.get_scan() == (i + 1, 0))
