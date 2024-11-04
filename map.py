4ab3c128ec80680b9535bd858fd2b8dcadac16da
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-04 12:27:06 +0000

    test_map_height defined

--- map.py
+++ map.py
@@ -1,6 +1,12 @@
 class Map:
     def __init__(self, data):
-        self.__width = len(data)
+        substrings = str.splitlines(data)
+
+        self.__width = len(substrings[0])
+        self.__height = len(substrings)
 
     def get_width(self):
         return self.__width
+
+    def get_height(self):
+        return self.__height
--- test_map.py
+++ test_map.py
@@ -6,3 +6,10 @@ def test_map_width():
     for i in range(1, 10):
         map_data += 'x'
         assert(Map(map_data).get_width() == i)
+
+def test_map_height():
+    map_data = ""
+
+    for i in range(1, 10):
+        map_data += '\n'
+        assert(Map(map_data).get_height() == i)
