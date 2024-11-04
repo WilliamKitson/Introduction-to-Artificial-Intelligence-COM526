5c407e487816e838140afd83c888660115df52bc
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-04 14:27:59 +0000

    version1 redefined

--- version1.py
+++ version1.py
@@ -5,13 +5,14 @@ import time
 class Version1:
     def __init__(self, map_data):
         self.__map = Map(map_data)
-        self.__cleaner = Cleaner(0, 0)
+        self.__cleaner = Cleaner(1, 1)
 
     def execute(self):
         print(f"map width: {self.__map.get_width()}, height: {self.__map.get_height()}")
 
         while self.__cleaner.get_battery():
-            self.__cleaner.set_scan(0)
+            self.__cleaner.set_scan(self.__map.get_blocked(self.__cleaner.get_scan()[0], self.__cleaner.get_scan()[1]))
             self.__cleaner.cycle()
+
             print(f"cycle {self.__cleaner.get_cycle()}: {self.__cleaner.get_render()} ({self.__cleaner.get_battery()}%)")
             time.sleep(1)
