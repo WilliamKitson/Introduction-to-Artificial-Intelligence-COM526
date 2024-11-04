ee84b80816308e35f173777b66de00afe1a416b8
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-04 14:06:27 +0000

    test_node_blocked defined

--- map.py
+++ map.py
@@ -1,12 +1,20 @@
 class Map:
     def __init__(self, data):
         substrings = str.splitlines(data)
+        width = len(substrings[0])
+        height = len(substrings)
 
-        self.__width = len(substrings[0])
-        self.__height = len(substrings)
+        self.__data = [["" for i in range(width)] for j in range(height)]
+
+        for i in range(height):
+            for j in range(width):
+                self.__data[i][j] = substrings[i][j]
 
     def get_width(self):
-        return self.__width
+        return len(self.__data[0])
 
     def get_height(self):
-        return self.__height
+        return len(self.__data)
+
+    def get_blocked(self, x, y):
+        return self.__data[x][y] == "x"
--- test_map.py
+++ test_map.py
@@ -13,3 +13,26 @@ def test_map_height():
     for i in range(1, 10):
         map_data += '\n'
         assert(Map(map_data).get_height() == i)
+
+def test_node_blocked():
+    width = 10
+    height = 10
+
+    for i in range(0, width):
+        for j in range(0, height):
+            assert(Map(generate_test_map(width, height, i, j, "x")).get_blocked(i, j) == True)
+
+def generate_test_map(width, height, x, y, char):
+    map_data = ""
+
+    for i in range(width):
+        for j in range(height):
+            if (i,j) == (x,y):
+                map_data += char
+
+            else:
+                map_data += '0'
+
+        map_data += '\n'
+
+    return map_data
