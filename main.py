1b8cc40a131b03afbe64c944478756eebd247f3c
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-04 16:21:26 +0000

    version1 redefined

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from version1 import Version1
 
-def load_map():
-    with open("map_data.txt", 'r') as file:
+def load_map(filepath):
+    with open(filepath, 'r') as file:
         file_content = file.read()
 
     return file_content
 
-Version1(
-    "xxxxxxxxxx\n"
-    "x        x\n"
-    "x        x\n"
-    "xxxxxxxxxx\n"
-).execute()
+Version1(load_map("maps/simple_map.txt")).execute()
