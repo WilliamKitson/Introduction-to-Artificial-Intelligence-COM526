92c1a48ea69e7b4909c223f2acda8852cca3c42b
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-10 13:13:20 +0000

    test_render_east defined

--- charger.py
+++ charger.py
@@ -1,6 +1,13 @@
 class Charger:
     def __init__(self, x_position, y_position):
-        pass
+        self.__direction = 0
+
+    def turn(self):
+        self.__direction = 1
 
     def render(self):
-        return "u"
+        if self.__direction == 0:
+            return "u"
+
+        if self.__direction == 1:
+            return "i"
--- test_charger.py
+++ test_charger.py
@@ -2,3 +2,9 @@ from charger import Charger
 
 def test_render_default():
     assert(Charger(0, 0).render() == "u")
+
+def test_render_east():
+    charger = Charger(0, 0)
+    charger.turn()
+
+    assert(charger.render() == "i")
