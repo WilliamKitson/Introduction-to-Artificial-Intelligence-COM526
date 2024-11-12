4dde269f2ff819dbcdb3705eb7c06944108a8c6b
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-12 15:02:01 +0000

    maps directory defined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from version1 import Version1
 
-with open("COM526_map_simple.txt", 'r') as file:
+with open("maps/COM526_map_simple.txt", 'r') as file:
     file_content = file.read()
 
 Version1(file_content).execute()
