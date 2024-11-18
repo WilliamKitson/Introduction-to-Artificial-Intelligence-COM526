86a5ab05605874f88e4b6d1fedb3d673b9dc7de0
4kitsw10 <4kitsw10@solent.ac.uk>
2024-11-18 15:03:28 +0000

    pathfinder test_add_node defined

--- version_2/Pathfinder.py
+++ version_2/Pathfinder.py
@@ -1,9 +1,19 @@
 class Pathfinder:
     def __init__(self, start):
+        self.__nodes = []
         self.__start = start
+
+    def add_node(self, node):
+        self.__nodes.append(node)
 
     def render(self, position):
         if position == self.__start:
             return " "
 
         return "?"
+
+    def get_node(self, position):
+        for i in self.__nodes:
+            if i[0] == position[0]:
+                if i[1] == position[1]:
+                    return i[2]
--- version_2/test_pathfinder.py
+++ version_2/test_pathfinder.py
@@ -1,5 +1,13 @@
 from version_2.Pathfinder import Pathfinder
 
+def test_add_node():
+    pathfinder = Pathfinder((0, 0))
+
+    for i in range(0, 10):
+        for j in range(0, 10):
+            pathfinder.add_node((i, j, i + j))
+            assert(pathfinder.get_node((i, j)) == i + j)
+
 def test_render_default():
     start = (0, 0)
     assert(Pathfinder(start).render(start) == " ")
