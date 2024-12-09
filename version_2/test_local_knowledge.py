21f842fe9c13951ad1abd1148b6bbbe9b75b56fc
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-09 14:19:40 +0000

    test_scan_zone_south redefined

--- version_2/test_local_knowledge.py
+++ version_2/test_local_knowledge.py
@@ -62,3 +62,13 @@ def test_scan_zone_east():
             pathfinder.add_free(position_east)
             pathfinder.explore((i, j))
             assert(pathfinder.get_node(position_east) == " ")
+
+def test_scan_zone_south():
+    for i in range(0, 10):
+        for j in range(0, 10):
+            pathfinder = LocalKnowledge()
+            position_south = tuple(map(sum, zip((i, j), (1, 0))))
+
+            pathfinder.add_free(position_south)
+            pathfinder.explore((i, j))
+            assert(pathfinder.get_node(position_south) == " ")
