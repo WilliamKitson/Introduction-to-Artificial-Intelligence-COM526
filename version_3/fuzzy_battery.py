1d9eefbb7ed372589beeac9e5f1b31a5f55d5f78
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-09 17:40:17 +0000

    fuzzy_battery.py redefined

--- version_3/fuzzy_battery.py
+++ version_3/fuzzy_battery.py
@@ -6,16 +6,26 @@ from skfuzzy import control as ctrl
 
 class FuzzyBattery:
     def __init__(self):
-        self.__fan_speed = ctrl.Antecedent(np.arange(0, 100, 1), "fan speed")
-        self.__fan_speed["slow"] = fuzz.zmf(self.__fan_speed.universe, 1, 50)
-        self.__fan_speed["medium"] = fuzz.trapmf(self.__fan_speed.universe, [25, 50, 50, 75])
-        self.__fan_speed["fast"] = fuzz.smf(self.__fan_speed.universe, 50, 100)
-
         self.__battery_drain = ctrl.Consequent(np.arange(0, 10, 1), "battery drain")
+        self.__initialise_battery_drain()
+
+        self.__fan_speed = ctrl.Antecedent(np.arange(0, 100, 1), "fan speed")
+        self.__initialise_fan_speed()
+
+        self.__battery_drain_control = self.__initialise_battery_drain_control()
+        self.__battery_drain_sim = ctrl.ControlSystemSimulation(self.__battery_drain_control)
+
+    def __initialise_battery_drain(self):
         self.__battery_drain["slow"] = fuzz.zmf(self.__battery_drain.universe, 0, 4)
         self.__battery_drain["medium"] = fuzz.trapmf(self.__battery_drain.universe, [2, 4, 6, 8])
         self.__battery_drain["fast"] = fuzz.smf(self.__battery_drain.universe, 6, 10)
 
+    def __initialise_fan_speed(self):
+        self.__fan_speed["slow"] = fuzz.zmf(self.__fan_speed.universe, 1, 50)
+        self.__fan_speed["medium"] = fuzz.trapmf(self.__fan_speed.universe, [25, 50, 50, 75])
+        self.__fan_speed["fast"] = fuzz.smf(self.__fan_speed.universe, 50, 100)
+
+    def __initialise_battery_drain_control(self):
         drain_slow = ctrl.Rule(
             self.__fan_speed["slow"],
             self.__battery_drain["slow"]
@@ -31,13 +41,11 @@ class FuzzyBattery:
             self.__battery_drain["fast"]
         )
 
-        self.__battery_drain_control =  ctrl.ControlSystem([
+        return ctrl.ControlSystem([
             drain_slow,
             drain_medium,
             drain_fast
         ])
-
-        self.__battery_drain_sim = ctrl.ControlSystemSimulation(self.__battery_drain_control)
 
     def calculate(self, fan_speed):
         self.__battery_drain_sim.input["fan speed"] = fan_speed
---
+++ version_3/fuzzy_cleaning.py
@@ -0,0 +1,11 @@
+#  Copyright (c) 2024. William E. Kitson
+
+class FuzzyCleaning:
+    def __init__(self):
+        pass
+
+    def calculate(self):
+        pass
+
+    def get_cleaning_rate(self):
+        pass
