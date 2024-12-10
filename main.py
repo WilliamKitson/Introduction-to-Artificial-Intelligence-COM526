a276c15cd510c09f97d558f75959fc3b0ca166ce
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-10 13:31:34 +0000

    pathfinder.py defined

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from version_2.demonstration_version_2 import DemonstrationVersion2
 with open("maps/COM526_map_simple.txt", 'r') as file:
     file_content = file.read()
 
-DemonstrationVersion1(file_content).execute()
+#DemonstrationVersion1(file_content).execute()
 DemonstrationVersion2(file_content).execute()
 
 from version_3.fuzzy_fan import FuzzyFan
