abd3ef210425e7e7d6e1aa9eb5c09f0372e1581f
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-10 14:33:53 +0000

    local_knowledge.py redefined

--- version_2/test_local_knowledge.py
+++ version_2/test_local_knowledge.py
@@ -3,106 +3,106 @@
 from version_2.local_knowledge import LocalKnowledge
 
 def test_empty_node():
-    pathfinder = LocalKnowledge()
+    local_knowledge = LocalKnowledge()
 
     for i in range(1, 10):
         for j in range(1, 10):
-            assert(pathfinder.get_node((i, j)) == "?")
+            assert(local_knowledge.get_node((i, j)) == "?")
 
 def test_explored_free():
-    pathfinder = LocalKnowledge()
+    local_knowledge = LocalKnowledge()
 
     for i in range(1, 10):
         for j in range(1, 10):
-            pathfinder.add_free((i, j))
-            pathfinder.explore((i, j))
-            assert(pathfinder.get_node((i, j)) == " ")
+            local_knowledge.add_free((i, j))
+            local_knowledge.explore((i, j))
+            assert(local_knowledge.get_node((i, j)) == " ")
 
 def test_unexplored_free():
-    pathfinder = LocalKnowledge()
+    local_knowledge = LocalKnowledge()
 
     for i in range(1, 10):
         for j in range(1, 10):
-            pathfinder.add_free((i, j))
-            assert(pathfinder.get_node((i, j)) == "?")
+            local_knowledge.add_free((i, j))
+            assert(local_knowledge.get_node((i, j)) == "?")
 
 def test_explored_blocked():
-    pathfinder = LocalKnowledge()
+    local_knowledge = LocalKnowledge()
 
     for i in range(1, 10):
         for j in range(1, 10):
-            pathfinder.add_blocked((i, j))
-            pathfinder.explore((i, j))
-            assert(pathfinder.get_node((i, j)) == "x")
+            local_knowledge.add_blocked((i, j))
+            local_knowledge.explore((i, j))
+            assert(local_knowledge.get_node((i, j)) == "x")
 
 def test_unexplored_blocked():
-    pathfinder = LocalKnowledge()
+    local_knowledge = LocalKnowledge()
 
     for i in range(1, 10):
         for j in range(1, 10):
-            pathfinder.add_blocked((i, j))
-            assert(pathfinder.get_node((i, j)) == "?")
+            local_knowledge.add_blocked((i, j))
+            assert(local_knowledge.get_node((i, j)) == "?")
 
 def test_explored_charger():
-    pathfinder = LocalKnowledge()
+    local_knowledge = LocalKnowledge()
 
     for i in range(1, 10):
         for j in range(1, 10):
-            pathfinder.add_charger((i, j))
-            pathfinder.explore((i, j))
-            assert(pathfinder.get_node((i, j)) == "u")
+            local_knowledge.add_charger((i, j))
+            local_knowledge.explore((i, j))
+            assert(local_knowledge.get_node((i, j)) == "u")
 
 def test_unexplored_charger():
-    pathfinder = LocalKnowledge()
+    local_knowledge = LocalKnowledge()
 
     for i in range(1, 10):
         for j in range(1, 10):
-            pathfinder.add_charger((i, j))
-            assert(pathfinder.get_node((i, j)) == "?")
+            local_knowledge.add_charger((i, j))
+            assert(local_knowledge.get_node((i, j)) == "?")
 
 def test_scan_zone_north():
     for i in range(0, 10):
         for j in range(0, 10):
-            pathfinder = LocalKnowledge()
+            local_knowledge = LocalKnowledge()
             position_north = tuple(map(sum, zip((i, j), (-1, 0))))
 
-            pathfinder.add_free(position_north)
-            pathfinder.explore((i, j))
-            assert(pathfinder.get_node(position_north) == " ")
+            local_knowledge.add_free(position_north)
+            local_knowledge.explore((i, j))
+            assert(local_knowledge.get_node(position_north) == " ")
 
 def test_scan_zone_east():
     for i in range(0, 10):
         for j in range(0, 10):
-            pathfinder = LocalKnowledge()
+            local_knowledge = LocalKnowledge()
             position_east = tuple(map(sum, zip((i, j), (0, 1))))
 
-            pathfinder.add_free(position_east)
-            pathfinder.explore((i, j))
-            assert(pathfinder.get_node(position_east) == " ")
+            local_knowledge.add_free(position_east)
+            local_knowledge.explore((i, j))
+            assert(local_knowledge.get_node(position_east) == " ")
 
 def test_scan_zone_south():
     for i in range(0, 10):
         for j in range(0, 10):
-            pathfinder = LocalKnowledge()
+            local_knowledge = LocalKnowledge()
             position_south = tuple(map(sum, zip((i, j), (1, 0))))
 
-            pathfinder.add_free(position_south)
-            pathfinder.explore((i, j))
-            assert(pathfinder.get_node(position_south) == " ")
+            local_knowledge.add_free(position_south)
+            local_knowledge.explore((i, j))
+            assert(local_knowledge.get_node(position_south) == " ")
 
 def test_scan_zone_west():
     for i in range(0, 10):
         for j in range(0, 10):
-            pathfinder = LocalKnowledge()
+            local_knowledge = LocalKnowledge()
             position_west = tuple(map(sum, zip((i, j), (0, -1))))
 
-            pathfinder.add_free(position_west)
-            pathfinder.explore((i, j))
-            assert(pathfinder.get_node(position_west) == " ")
+            local_knowledge.add_free(position_west)
+            local_knowledge.explore((i, j))
+            assert(local_knowledge.get_node(position_west) == " ")
 
 def test_get_charger():
     for i in range(1, 10):
         for j in range(1, 10):
-            pathfinder = LocalKnowledge()
-            pathfinder.add_charger((i, j))
-            assert (pathfinder.get_charger() == (i, j))
+            local_knowledge = LocalKnowledge()
+            local_knowledge.add_charger((i, j))
+            assert (local_knowledge.get_charger() == (i, j))
