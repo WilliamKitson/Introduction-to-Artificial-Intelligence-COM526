fc99d7513bda8f75914b0d376432b28b7cd9deea
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-15 14:43:09 +0000

    local_knowledge.py redefined

--- version_2/local_knowledge.py
+++ version_2/local_knowledge.py
@@ -15,6 +15,9 @@ class LocalKnowledge:
         self.__nodes.append((position[0], position[1], 0, "u"))
 
     def update_free(self, position, dirt):
+        if self.get_node(position) in "x":
+            return
+
         free_node_index = self.__get_node_index_at(position)
         free_node = self.__nodes[free_node_index]
 
--- version_2/test_local_knowledge.py
+++ version_2/test_local_knowledge.py
@@ -131,7 +131,17 @@ def test_update_free():
 
             assert(local_knowledge.get_node((i, j)) == str(i + j))
 
-# test you cannot update non free nodes
+def test_update_blocked():
+    local_knowledge = LocalKnowledge()
 
+    for i in range(1, 10):
+        for j in range(1, 10):
+            local_knowledge.add_blocked((i, j))
+            local_knowledge.explore((i, j))
+            local_knowledge.update_free((i, j), (i + j))
+
+            assert(local_knowledge.get_node((i, j)) == "x")
+
+# test update charger
 # test node queue prioretises the highest dirt value
 # test node queue prioretises unexplored when no dirt is detcted
